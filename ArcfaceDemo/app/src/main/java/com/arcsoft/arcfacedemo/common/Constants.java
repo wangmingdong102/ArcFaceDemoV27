f735adaa508a279cf3c2bbb6db69b9f16f064f1d
wangmingdong102 <wangmingdong102@sina.com>
2019-03-26 14:25:11 +0800

    init

---
+++ ArcfaceDemo/app/src/main/java/com/arcsoft/arcfacedemo/common/Constants.java
@@ -0,0 +1,6 @@
+package com.arcsoft.arcfacedemo.common;
+
+public class Constants {
+    public static final String APP_ID = "F23htSu7WfVe8qgEjTLYvETuzjkVwD5KHnc7vL4AaKWH";
+    public static final String SDK_KEY = "3nvqQMGk9uiU4Kb1inZhbX1eh9NA1MAE5Rgh3mrWE46z";
+}
